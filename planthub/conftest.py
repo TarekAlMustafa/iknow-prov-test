c088be8d7a446790e44c260439d2c201a58a8aad
Franzi <franzi82@gmx.de>
2021-08-16 11:21:11 +0200

    Initial commit

---
+++ planthub/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from planthub.users.models import User
+from planthub.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
