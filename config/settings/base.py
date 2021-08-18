ba1266f2af3a2d96d6ae060fd167e06a1be2ee9e
Franzi <franzi82@gmx.de>
2021-08-18 10:42:35 +0200

    remove empty lines

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,9 +41,6 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
-
-
-
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
