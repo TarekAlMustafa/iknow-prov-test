66d9ba9c9e866bdd163394f9a461c7955d5390bd
Franzi <franzi82@gmx.de>
2021-09-02 21:27:10 +0200

    Add full path to module #3

--- planthub/datasets/admin.py
+++ planthub/datasets/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-from datasets.models import Dataset, File, Datastructure, DatastructureInline
+from planthub.datasets.models import Dataset, File, Datastructure, DatastructureInline
 #from suit.admin import SortableListForm
 
 
