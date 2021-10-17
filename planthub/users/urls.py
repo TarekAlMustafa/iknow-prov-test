13fce6d586ba27b3f41f22cae964acaccba2ef69
Franzi <franzi82@gmx.de>
2021-10-17 22:05:12 +0200

    Add auth via django-knox and JWT #1, #2

--- planthub/users/urls.py
+++ planthub/users/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path  # ,include
+from knox import views as knox_views
 
 from planthub.users.views import user_detail_view, user_redirect_view, user_update_view
+
+from .api.api import LoginAPI, RegistrationAPI, UserApi
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
+    # path('api/auth', include('knox.urls')), # not sure if we need this
+    path('register', RegistrationAPI.as_view()),
+    path('login', LoginAPI.as_view()),
+    path('user', UserApi.as_view()),
+    path('logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
