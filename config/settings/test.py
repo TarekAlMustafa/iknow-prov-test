5b9efdda29263c6a771984af60cbe6c08133e7a1
Franziska Zander <franziska.zander@uni-jena.de>
2022-01-22 17:13:51 +0000

    Update test.py

--- config/settings/test.py
+++ config/settings/test.py
@@ -5,6 +5,9 @@ With these settings, tests run faster.
 from .base import *  # noqa
 from .base import env
 
+# Overwrite database settings
+DATABASES = {"default": env.db("DATABASE_URL")}
+
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
