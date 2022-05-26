e7db63671c7d96a08ee90c04b90c01fc9cb89b8d
Franzi <franzi82@gmx.de>
2022-05-26 12:32:03 +0200

    Change path for images #31

--- planthub/projects/models.py
+++ planthub/projects/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 class ProjectContact(models.Model):
     person_name = models.CharField(_("Name"), max_length=200)
     person_email = models.CharField(_("Email"), max_length=100)
-    image = models.FileField(_("File"), upload_to='person_images/', null=True)
+    image = models.FileField(_("File"), upload_to='images/person_images/', null=True)
 
     def __str__(self):
         return self.person_name
@@ -19,7 +19,7 @@ class Project(models.Model):
     sub_title_de = models.CharField(_("Subtitle in German"), max_length=500)
     description_en = models.CharField(_("Description in English"), max_length=2000)
     description_de = models.CharField(_("Description in German"), max_length=2000)
-    logo = models.FileField(_("Project Logo"), upload_to='project_logos/')
+    logo = models.FileField(_("Project Logo"), upload_to='images/project_logos/')
     link = models.CharField(_("Project Website"), max_length=1000)
     contact = models.ManyToManyField(ProjectContact, related_name="project_project_contacts",
                                      blank=True, verbose_name=_("Project contacts"))
