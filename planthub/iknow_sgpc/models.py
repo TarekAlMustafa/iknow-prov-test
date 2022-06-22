c8b8fd1c2e2bff8b447b646e40a5f0bb415beb81
ne34jup <erik.kleinsteuber@uni-jena.de>
2022-06-22 19:18:52 +0200

    initial commit for iknow graphcreation backend features

---
+++ planthub/iknow_sgpc/models.py
@@ -0,0 +1,33 @@
+# from django.conf import settings
+from django.db import models
+
+from planthub.iknow_sgp.models import SGP
+
+# Create your models here.
+
+# https://www.tutorialspoint.com/adding-json-field-in-django-models
+# https://riptutorial.com/django/example/30649/foreignkey
+# https://stackoverflow.com/questions/34305805/foreignkey-user-in-models
+
+PROJECTDOMAIN_CHOICES = [
+    ('PhenObs', 'Phenobs'),
+    ('Glonaf', 'Glonaf'),
+    ('sPlot', 'sPlot')
+]
+
+
+class SGPC(models.Model):
+    # name of this collection (user convenience when continuing work)
+    collectionname = models.CharField(max_length=255, default='')
+
+    # name of the associated biology research project (same for the associated sgps)
+    bioprojectname = models.CharField(max_length=255, choices=PROJECTDOMAIN_CHOICES, default='PhenObs')
+
+    # description of the collection (user convenience)
+    description = models.CharField(max_length=2000, default='')
+
+    # all associated sgprojects (should be only assigned once and not changed afterwards)
+    associated_sgprojects = models.ManyToManyField(SGP)
+
+    # later for access control and displaying user specific database entries etc.
+    # owningUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, default=1)
